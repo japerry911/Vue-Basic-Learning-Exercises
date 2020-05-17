f137e8a88ac5ee08509b21cc6a2be8948eb4ddb9
japerry911 <jack.e.perry@outlook.com>
2020-05-17 05:54:50 -0600

    Assignment5

---
+++ Assignment5/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        show: true,
+        show2: true,
+        arraySky: ['sky', 'dog', 'great', 'bestest'],
+        object1: { title: 'Lord of the Rings', author: 'J.R.R Tolkiens', books: '3' },
+        object2: { name: 'TEST OBJECT', data: [1.67, 1.33, 0.98, 2.21] }
+    }
+});
