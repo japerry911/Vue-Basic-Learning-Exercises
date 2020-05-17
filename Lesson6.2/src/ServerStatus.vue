4acec86c12151284d1e5302367f66251bd6f22fb
japerry911 <jack.e.perry@outlook.com>
2020-05-17 12:39:58 -0600

    Lesson6.2

---
+++ Lesson6.2/src/ServerStatus.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Server Status: {{ status }}</p>
+        <button @click='changeStatus'>Change</button>
+    </div>
+</template>
+
+<script>
+    export default {
+    data: function() {
+        return {
+        status: 'Critical' 
+        }
+    },
+    methods: {
+        changeStatus() {
+        this.status = 'Normal';
+        }
+    }
+    }
+</script>
+
+<style>
+</style>
