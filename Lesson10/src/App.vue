956a0d6b20d75954cfa58711cd3bf5b75a8ed952
japerry911 <jack.e.perry@outlook.com>
2020-05-18 17:04:12 -0600

    Lesson10 Completed

--- Lesson10/src/App.vue
+++ Lesson10/src/App.vue
@@ -2,8 +2,22 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
+                <h1>Built-in Directives</h1>
+                <p v-text="'Some Text'"></p>
+                <p v-html="'<strong>Strong Text</strong>'"></p>
+            </div>
+            <hr>
+            <div class='row'>
+                <div class='col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3'>
+                    <h1>Custom Directives</h1>
+                    <p v-highlight:background.delayed="'red'">Color this</p>
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3'>
+                    <h1>Local Custom Directives</h1>
+                    <p v-localHighlight:background.delayed.blink="{mainColor: 'red', secondColor: 'yellow', delay: 500}">Color this</p>
+                </div>
             </div>
         </div>
     </div>
@@ -11,6 +25,36 @@
 
 <script>
     export default {
+        directives: {
+            localHighlight: {
+                bind(el, binding, vnode) {
+                    //el.style.backgroundColor = 'limegreen';
+                    var delay = 0;
+
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+
+                    if (binding.modifiers['blink']) {
+                        setTimeout(() => {
+                            var bool = true;
+                            setInterval(() => {
+                                if (binding.arg === 'background') {
+                                    el.style.backgroundColor = bool ? binding.value.mainColor : binding.value.secondColor;
+                                }
+                                bool = !bool;
+                            }, binding.value.delay)
+                        }, delay);
+                    } else {
+                        setTimeout(() => {
+                            if (binding.arg === 'background') {
+                                el.style.backgroundColor = binding.value;
+                            }
+                        }, delay);
+                    }
+                }
+            }
+        }
     }
 </script>
 
