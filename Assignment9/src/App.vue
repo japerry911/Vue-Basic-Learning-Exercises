d0dc8fd0d2cbbcf9080b38ac0c12377fe252f7b4
japerry911 <jack.e.perry@outlook.com>
2020-05-18 16:26:38 -0600

    Assignment9 Completed

--- Assignment9/src/App.vue
+++ Assignment9/src/App.vue
@@ -1,37 +1,73 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if='!isSubmitted'>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <h1>Signup Form</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="first-name">First Name</label>
+                        <input
+                            type="text"
+                            id="first-name"
+                            class="form-control"
+                            v-model='userData.firstName'
+                        >
+                    </div>
+                    <div class='form-group'>
+                        <label for='last-name'>Last Name</label>
+                        <input 
+                            type='text'
+                            id='last-name'
+                            class='form-control'
+                            v-model='userData.lastName'
+                        >
+                    </div>
+                    <div class='form-group'>
+                        <label for='email'>Email</label>
+                        <input
+                            type='email'
+                            id='email'
+                            class='form-control'
+                            v-model='userData.email'
+                        >
+                    </div>
+                    <div class='form-group'>
+                        <label for='password'>Password</label>
+                        <input
+                            type='password'
+                            id='password'
+                            class='form-control'
+                            v-model='userData.password'
+                        >
+                    </div>
+                    <switched v-model='storeSwitch'></switched>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                        class="btn btn-primary"
+                        @click.prevent='submit'
+                    >
+                        Submit!
+                    </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if='isSubmitted'>
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userData.firstName }} {{ userData.lastName }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storeSwitch ? 'Yes' : 'No' }}</p>
                     </div>
                 </div>
             </div>
@@ -40,8 +76,30 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
+
     export default {
-    }
+        data() {
+            return {
+                userData: {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    password: ''
+                },
+                storeSwitch: true,
+                isSubmitted: false 
+            };
+        },
+        components: {
+            switched: Switch
+        },
+        methods: {
+            submit() {
+                this.isSubmitted = true;
+            }
+        }
+    };
 </script>
 
 <style>
