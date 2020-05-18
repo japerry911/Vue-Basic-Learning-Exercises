b6c049c8d49f601662694777fbce55489c609ed1
japerry911 <jack.e.perry@outlook.com>
2020-05-18 10:12:21 -0600

    Lesson8 completed

--- Lesson8/src/App.vue
+++ Lesson8/src/App.vue
@@ -2,16 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+                <button @click='selectedComponent = "app-quote"'>Quote</button>
+                <button @click='selectedComponent = "app-author"'>Author</button>
+                <button @click='selectedComponent = "app-new"'>New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is='selectedComponent'>
+                        Default Content
+                    </component>
+                </keep-alive>
+                <!-- <app-quote>
+                    <h2 slot='title'>{{ quoteTitle }}</h2>
+                    <span style='color: #ccc' slot='subtitle'>The Replacement Subtitle</span>
+                    <p slot='content'>A Wonderful Quote</p>
+                </app-quote> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
-        
+        data: function() {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'app-quote'
+            };
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
     }
 </script>
 
---
+++ Lesson8/src/components/New.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class='box'>
+        <h3>New Quote</h3>
+        <button @click='counter++'>Click me!</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>   
+    export default {
+        data: function() {
+            return {
+                counter: 0
+            };
+        },
+        deactivated() {
+            console.log('deactivated');
+        },
+        activated() {
+            console.log('activated');
+        }
+    };
+</script>
