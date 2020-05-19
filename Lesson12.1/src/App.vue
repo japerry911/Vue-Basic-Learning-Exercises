1d8f5a0f36f51649b0a9319551d0e820203c344f
japerry911 <jack.e.perry@outlook.com>
2020-05-19 12:50:29 -0600

    Lesson12.1 progress, will complete later, moving on to router

--- Lesson12.1/src/App.vue
+++ Lesson12.1/src/App.vue
@@ -3,7 +3,18 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class='btn btn-primary' @click='show = !show'>Show Alert</button>
+                <br><br>
+                <transition name='fade'>
+                    <div class='alert alert-info' v-if='show'>This is some info</div>
+                </transition>
+                <transition name='slide' type='animation'>
+                    <div class='alert alert-info' v-if='show'>This is some info</div>
+                </transition>
+                <transition name='slide' type='animation' appear>
+                    <div class='alert alert-info' v-if='show'>This is some info</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -13,12 +24,66 @@
     export default {
         data() {
             return {
-
+                show: true 
             }
         }
     }
 </script>
 
-<style>
+<style scoped>
+    .fade-enter {
+        opacity: 0;
+    }
 
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+        /* default already -> opacity: 1; */
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter {
+        /* already set up as keyframe -> transform: translateY(20px); */
+        opacity: 0;
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+
+    .slide-leave {
+        /* already set up as keyframe -> transform: translateY(0); */
+        /* default opacity already set -> opacity: 1; */
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 3s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
 </style>
